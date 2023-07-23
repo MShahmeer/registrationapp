17e986bed102300275fb1038b1ce6b48be09e4c8
Muhammad Shahmeer <91695394+MShahmeer@users.noreply.github.com>
2023-07-23 16:08:03 +0500

    Registration Form in angular
    Practicing angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'registrationapp';
+
+  constructor(private toaster: ToastrService) {}
+
+  ngOnInit() {
+    // this.toaster.success('saved successfully', 'User master');
+    // Swal.fire('Hello World');
+    // Swal.fire("oops..", "something went wrong", "error")
+    // Swal.fire("Thanks", "Form saved", "success")
+  }
 }
